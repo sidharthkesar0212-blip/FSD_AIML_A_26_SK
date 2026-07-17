0e094ffc7870c3acfd53f700c7bdaf7c82c57801
Sidharth kesar <sidharthkesar0212@gmail.com>
2026-07-17 14:20:14 +0530

    Created ex1.js file

---
+++ ex1.js
@@ -0,0 +1,7 @@
+// let a = 10;
+// const b = 25;
+// console.log("a = " + a);
+// console.log("b = " + b);
+const sum = (a,b) => a + b;
+console.log("sum = " + sum(10, 25));
+console.log("sum = " + sum(35, 98));
